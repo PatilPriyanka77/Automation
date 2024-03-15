ce4b5484205e2281d6d2a535b5b0d2584023b756
SAGAR DESHMUKH <SAGAR DESHMUKH@DESKTOP-Q6F4DSB>
2024-03-15 11:10:03 +0530

    asach

--- Facebook/src/main/java/Pages/LoginOrSignUpPage.java
+++ Facebook/src/main/java/Pages/LoginOrSignUpPage.java
@@ -7,34 +7,34 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginOrSignUpPage {
 	
-	@FindBy (xpath = "//input[@id='email']")
-	private WebElement userName;
-	
-	@FindBy (xpath = "//input[@id='pass']")
-	private WebElement password;
-	
-	@FindBy (xpath = "//button[@id='loginbutton']")
-	private WebElement loginButton;
-	
-	@FindBy (xpath = "//a[text()='Messenger']")
-	private WebElement messagener;
-	
-	
+//	@FindBy (xpath = "//input[@id='email']")
+//	private WebElement userName;
+//	
+//	@FindBy (xpath = "//input[@id='pass']")
+//	private WebElement password;
+//	
+//	@FindBy (xpath = "//button[@id='loginbutton']")
+//	private WebElement loginButton;
+//	
+//	@FindBy (xpath = "//a[text()='Messenger']")
+//	private WebElement messagener;
+//	
+//	
 	public LoginOrSignUpPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 		
 	}
     
-	public void loginToApplication() {
-		userName.sendKeys("priyanka.patil@gmail.com");
-		password.sendKeys("priya123");
-		loginButton.click();
-		
-	}
-	
-	public void openMessenger() {
-		messagener.click();
-		
-	 }
+//	public void loginToApplication() {
+//		userName.sendKeys("priyanka.patil@gmail.com");
+//		password.sendKeys("priya123");
+//		loginButton.click();
+//		
+//	}
+//	
+//	public void openMessenger() {
+//		messagener.click();
+//		
+//	 }
 		
 	}
